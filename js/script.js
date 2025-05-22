e239a9a32f1302c4ff58bc27e8395ff29e4a442b
Ain-Jeong <ain.jeong@stu.ocsb.ca>
2025-05-22 16:51:45 +0000

    init

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Ain Jeong All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Ain Jeong
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+'use strict'
+
+window.onload = function () {
+  // this calculates volume of a pyramid
+
+  const params = new URLSearchParams(document.location.search)
+
+  // input
+  const radius = params.get('radius')
+
+  console.log(radius)
+
+  // process
+  const volume = (4 / 3) * Math.PI * Math.pow(r, 3)
+  const dimension = '<ul>\n<li>a base = ' + radius
+  // output
+  document.getElementById('dimension').innerHTML = dimension
+  document.getElementById('volume').innerHTML = 'Volume is: ' + volume + ' mm²'
+}
